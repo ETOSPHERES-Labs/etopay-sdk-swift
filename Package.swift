139a088913e76d0d697d09951cc00f8beaa0cace
bot <etopaybot@etoshpheres.com>
2025-04-14 08:44:13 +0000

    Update Swift SDK package files from commit 48620b8e477f96169de890dcb104316d1c9e24aa

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "ETOPaySdkBin",
-			url: "https://github.com/ETOSPHERES-Labs/etopay-sdk-swift/releases/download/nightly/ETOPaySdkBin.xcframework-00a45f0.zip", checksum: "5c2095a25041a114e2f58d3a52cae3fbcba9917e0954dfcf866e1c4aa2a9e18d"
+			url: "https://github.com/ETOSPHERES-Labs/etopay-sdk-swift/releases/download/nightly/ETOPaySdkBin.xcframework-48620b8.zip", checksum: "77f9927494e7670b8cea4b2155b3c1957bd22a2d947f3cb2e9aeee5f1213f75c"
 		),
 		.target(
 			name: "ETOPaySdk",
